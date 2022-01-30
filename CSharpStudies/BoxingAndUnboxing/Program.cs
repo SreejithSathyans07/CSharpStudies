8df0cc911a4dd29ff474eb98da6244393015368f
Sreejith <sreejithsathyans@gmail.com>
2022-01-30 17:30:24 +0530

    Initial Commit 2 projects -> Boxing and Unboxing -> Delegates

---
+++ CSharpStudies/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*--------------------------
+
+- Boxing: Conversion of a value type in to reference type. It is implicit conversion.
+- Unboxing: Conversion of reference type in to value type. It should be manually done by the developer. It is explicit conversion.
+
+ ---------------------------*/
+
+namespace BoxingAndUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int boxingInt = 2000;
+            Object objBox = boxingInt;
+            Console.WriteLine(objBox);
+
+            Object objUnbox = 1000;
+            int unboxingInt = Convert.ToInt32(objUnbox);
+            Console.WriteLine(unboxingInt);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpStudies/Delegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*--------------------------
+
+- A delegate is a type safe function pointer, i.e. delegate points to a function that when you refer the delegate, the function will be invoked.
+- We can assign functions to a delegate which matches the signature.
+- Delegates are useful in implementing callbacks
+
+ ---------------------------*/
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderTheApplication obj = new RenderTheApplication();
+            obj.ThisMethodRunsVeryLong(PrintSquare);
+            Console.ReadKey();
+        }
+
+        public static void PrintSquare(int i)
+        {
+            Console.WriteLine(Math.Pow(i, 2));
+        }
+    }
+
+    public class RenderTheApplication
+    {
+        public delegate void delDisplayMessage(int i);
+        public void ThisMethodRunsVeryLong(delDisplayMessage callbackMethod)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                callbackMethod(i);
+            }
+        }
+    }
+}
